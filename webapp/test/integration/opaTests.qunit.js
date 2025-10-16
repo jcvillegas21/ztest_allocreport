5b0b7a068fec83f01f9cfc888361a8f6632935b7
jcvillegas21 <55837072+jcvillegas21@users.noreply.github.com>
2025-10-16 17:07:26 +0800

    Delete webapp directory

--- webapp/test/integration/opaTests.qunit.js
+++
@@ -1,7 +0,0 @@
-/* global QUnit */
-QUnit.config.autostart = false;
-
-sap.ui.require(["com/pg/s4/zaatpallocrpt/fiorizaatpallocrpt/test/integration/AllJourneys"
-], function () {
-	QUnit.start();
-});
